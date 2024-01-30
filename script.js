d26eefa21d3b86a16786ababe36273f1942c54cc
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-01-30 13:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function firstWord(s) {
   // your code here
-	let str = s.split(" ");
-	if(str.length===1){
-		return s;
+	s=s.trim();
+	s1 = s.split(" ");
+	if(s1.length>0){
+		return s1[0];
 	}
-	else{
-		return str[0];
-	}
+	return "";
 }
 // Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
 
